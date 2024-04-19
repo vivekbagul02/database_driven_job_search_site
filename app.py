52fb2c7aa70d6c71dd40dcab6be18ce1facfcc10
vivekbagul02 <129834079+vivekbagul02@users.noreply.github.com>
2024-04-19 06:36:04 +0000

    work in progress

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-print("my name is vivek")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home_page():
+
+  return render_template("home.html")
+
+
+if __name__ == "__main__":
+  app.run(host="0.0.0.0", debug=True)
