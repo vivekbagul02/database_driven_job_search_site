cf911d09ae532dcf17f52767920a08ac5b296601
vivekbagul02 <129834079+vivekbagul02@users.noreply.github.com>
2024-04-20 10:53:43 +0000

    almost ready to deploy

--- app.py
+++ app.py
@@ -1,12 +1,41 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+all_jobs_list = [{
+    "job_id": 101,
+    'position': 'Python Developer',
+    "location": "Pune,Maharashtra",
+    "salary": "Rs. 12,00,000"
+}, {
+    "job_id": 102,
+    'position': 'Data Scientist',
+    "location": "Pune,Maharashtra",
+    "salary": "Rs. 12,00,000"
+}, {
+    "job_id": 103,
+    'position': 'Python AWS',
+    "location": "Pune,Maharashtra",
+    "salary": "Rs. 12,00,000"
+}, {
+    "job_id": 104,
+    'position': 'Data Analyst',
+    "location": "Pune,Maharashtra",
+    "salary": "Rs. 12,00,000"
+}, {
+    "job_id": 105,
+    'position': 'Python Automation Engineer',
+    "location": "Pune,Maharashtra",
+    "salary": "Rs. 12,00,000"
+}]
+
 
 
 @app.route("/")
 def home_page():
+  all_jobs = jsonify(all_jobs_list)
 
-  return render_template("home.html")
+  return render_template("home.html", all_jobs_list=all_jobs_list)
 
 
 if __name__ == "__main__":
